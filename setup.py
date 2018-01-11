70e45da3a166846bb00a76ae7e3ca70adc88346e
Andrés Usán <a.usan@i2tic.com>
2018-01-11 13:26:27 +0100

    0.0.2

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 
 setup(
     name='django_loader_dumper',
-    packages=['django_loader_dumper'],
-    version='0.1',
+    packages=find_packages(),
+    version='0.0.2',
     description='Commands for loading and dumping fixtures based on app names.',
     author='Andruten',
     author_email='andusaben@gmail.com',
     url='https://github.com/andruten/django_loader_dumper',
-    download_url='https://github.com/andruten/django_loader_dumper/archive/0.1.tar.gz',
+    download_url='https://github.com/andruten/django_loader_dumper/archive/0.0.2.tar.gz',
     keywords=['django', 'fixtures', 'dumpdata', 'loaddata', 'commands', 'json'],
     classifiers=[],
 )
