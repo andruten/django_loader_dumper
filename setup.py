3ce9cff2949f082a1380d00bd3d49c14161fd366
Andrés Usán <a.usan@i2tic.com>
2018-01-11 11:26:55 +0100

    archive

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author='Andruten',
     author_email='andusaben@gmail.com',
     url='https://github.com/andruten/django_loader_dumper',
+    download_url='https://github.com/andruten/django_loader_dumper/archive/0.1.tar.gz',
     keywords=['django', 'fixtures', 'dumpdata', 'loaddata', 'commands', 'json'],
     classifiers=[],
 )
