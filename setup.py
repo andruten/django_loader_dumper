a2cfbdbea99b156280ebdc28bbe8558234b8dd59
Andrés Usán <a.usan@i2tic.com>
2018-01-12 14:31:08 +0100

    updated version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import find_packages
 setup(
     name='django_loader_dumper',
     packages=find_packages(),
-    version='1.0',
+    version='1.0.1',
     description='Commands for loading and dumping fixtures based on app names.',
     author='Andruten',
     author_email='andusaben@gmail.com',
     url='https://github.com/andruten/django_loader_dumper',
-    download_url='https://github.com/andruten/django_loader_dumper/archive/1.0.tar.gz',
+    download_url='https://github.com/andruten/django_loader_dumper/archive/1.0.1.tar.gz',
     keywords=['django', 'fixtures', 'dumpdata', 'loaddata', 'commands', 'json'],
     classifiers=[],
 )
